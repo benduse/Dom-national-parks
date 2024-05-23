a491aab1b3db728f916c435f84d6794681183df8
benduse <benjaminiyodusenga@gmail.com>
2024-05-23 01:17:31 -0500

    Commented out the older code. added code to create new element using createElement( ) method

--- index.js
+++ index.js
@@ -3,15 +3,28 @@ console.log(document);
 
 /**this statement will sellect al the desctiption on the page
  */
-const descriptions = document.querySelectorAll(".description-display");
+/**const descriptions = document.querySelectorAll(".description-display");
 
  const ratings = document.querySelectorAll(".rating-display .value");
  for (let rating of ratings) {
     let ratingValue = parseFloat(rating.innerText);
     
     if (ratingValue > 4.7){
-        rating.style.fontWeight ="bold";
-        rating.style.color = "#3ba17c";
+        rating.classList.add("high-rating");
+        rating.classList.remove("value");
     }
    
   }
+  */
+
+/**the code slects the park class as a selector on the page */
+  const parks = document.querySelectorAll(".park-display");
+
+  /** this code gives us the number of parks using the length property of the list */
+  const numberParks = parks.length;
+
+  /** creates an emply element using div */
+  const newElement = document.createElement("div");
+
+  /** the code below sets the text of the element created with innerTexxt property */
+  newElement.innerText = `${numberParks} exciting parks to visit`;
