60a0637c88a62e7a526db96e5995b632d53c4ca8
benduse <benjaminiyodusenga@gmail.com>
2024-05-23 00:53:36 -0500

    setting the elipses as clickable entities

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const descriptions = document.querySelectorAll(".description-display");
 
 for (let desc of descriptions.values()) {
     let content = desc.innerText;
-    
-    
+
     if (content.length > 250) {
         content = content.slice(0, 250);
-        content = content + "...";
+        content = content + '<a href="#">...</a>';
     }
-    console.log(content);
+    
+    desc.innerText = content;
   }
-  
+ 
