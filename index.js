892dfc5a3ad3db5aed1511f9b0b2b37e2f60cb38
benduse <benjaminiyodusenga@gmail.com>
2024-05-22 23:39:15 -0500

    deleted new json that was gonna let the file run on chrome

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ console.log(document);
 
 /**this statement will sellect al the desctiption on the page
  */
-const descriptions = document.querySelectorAll(".description-display");
+/*const descriptions = document.querySelectorAll(".description-display");*/
+
+for (let desc of descriptions.values()) {
+    let content = desc.innerText;
+    console.log(content);
+  }
