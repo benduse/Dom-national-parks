edb85cc1bd85b1d049437fe3f9149ff820649d8b
benduse <benjaminiyodusenga@gmail.com>
2024-05-23 00:42:49 -0500

    added elipses to the description that has more than 250 characters using slice method

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ console.log(document);
 
 /**this statement will sellect al the desctiption on the page
  */
-/*const descriptions = document.querySelectorAll(".description-display");*/
+const descriptions = document.querySelectorAll(".description-display");
+
+/** the code below addd ... on the content with more than two hundred and fity characters */
 
 for (let desc of descriptions.values()) {
     let content = desc.innerText;
+    
+    
+    if (content.length > 250) {
+        content = content.slice(0, 250);
+        content = content + "...";
+    }
     console.log(content);
   }
+  
