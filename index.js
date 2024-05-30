c24d073b1bd7ec508e46627d980ec69f49d896bd
benduse <benjaminiyodusenga@gmail.com>
2024-05-29 23:13:01 -0500

    DOM manipulation, Refactor the DOM manipulation code

--- index.js
+++ index.js
@@ -77,6 +77,80 @@ const allBtns = document.querySelectorAll(".rate-button");
 // Iterate through the list of buttons and add an event handler to each
 allBtns.forEach((btn) => {
   btn.addEventListener("click", (event) => {
-    console.log(event.target);
+    console.log(event.target.parentNode);
+    park.style.backgroundColor = "#c8e6c9";
   });
 });
+
+// Select the `nameSorter` link
+const nameSorter = document.querySelector("#name-sorter");
+
+// Add an event listener
+nameSorter.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log("You clicked the name sorter");
+});
+
+// Function for sorting by name
+const sortByName = (parkA, parkB) => {
+  const parkAName = parkA.querySelector("h2").innerText;
+  const parkBName = parkB.querySelector("h2").innerText;
+  if (parkAName < parkBName) {
+    return -1;
+  } else if (parkAName > parkBName) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+// Function for handling the `nameSorter` click
+const nameSorterClickHandler = (event) => {
+  event.preventDefault();
+
+  // 1.  Get the main element
+  const main = document.querySelector("main");
+
+  // 2. Get the list of parks
+  const parksList = main.querySelectorAll(".park-display");
+
+  // 3. Empty the main
+  main.innerHTML = "";
+
+  // 4. Create an array
+  const parksArray = Array.from(parksList);
+
+  // 5. Sort the array
+  parksArray.sort(sortByName);
+
+  // 6. Insert each park into the DOM
+  parksArray.forEach((park) => {
+    main.appendChild(park);
+  });
+};
+
+console.log("Before!");
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  console.log("Loaded!");
+});
+
+console.log("After!");
+
+// The code that runs once the DOM is loaded
+const init = () => {
+  // Select the `nameSorter` link
+  const nameSorter = document.querySelector("#name-sorter");
+
+  // Add an event listener
+  nameSorter.addEventListener("click", nameSorterClickHandler);
+
+  // Select the `ratingSorter` link
+  const ratingSorter = document.querySelector("#rating-sorter");
+
+  // Add an event listener
+  ratingSorter.addEventListener("click", ratingSorterClickHandler);
+}
+
+// Add event listener for `DOMContentLoaded`
+window.addEventListener("DOMContentLoaded", init);
