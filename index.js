a163f5310c315681927ed3231b00099431a406d9
benduse <benjaminiyodusenga@gmail.com>
2024-06-04 19:05:20 -0500

    added event listeners and a new park

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const submitHandler = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    console.log("The form was submitted");
+  };
 
+  const main = () => {
+    //get the form element 
+
+    const form = document.querySelector ("#park-form");
+
+    //Attach the submit handler
+
+form.addEventListener("submit", submitHandler );
+  };
+
+  window.addEventListener("DOMContentLoaded", main);
+
+  // Get the name input
+const parkName = document.querySelector("#name-input").value;
+
+console.log(parkName);
+
+const name = formData.get("name");
+console.log(name);
